65da2af5ed5339585b8ef9ba34c8b4057730a805
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-02-23 23:00:28 -0300

    praticando landing page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+import Main from './components/Main';
+import Conteudo from './components/Conteudo'
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+       <Conteudo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Conteudo.js
@@ -0,0 +1,25 @@
+export default function Conteudo(){
+    return(
+        <div className='conteudo'>
+        <div className='center'>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p>Aqui é o meu conteúdo de testes</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p>Aqui é o meu conteúdo de testes</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p>Aqui é o meu conteúdo de testes</p>
+          </div>
+          <div className='conteudo-single'>
+            <h3>Titulo da chamada</h3>
+            <p>Aqui é o meu conteúdo de testes</p>
+          </div>
+        </div>
+        
+      </div>
+    )
+}
